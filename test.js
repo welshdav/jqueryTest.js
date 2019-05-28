2e693f74a937d3766277b54b3341f89bd6a19f0d
David Welsh <welshdav@msu.edu>
2019-05-27 21:33:34 -0500

    Create test.js

---
+++ test.js
@@ -0,0 +1,15 @@
+ <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.0/jquery.min.js"></script>
+  <script type="text/javascript" src="script.js"></script>
+  <script>
+    
+  </script>
+
+window.onload = function() {
+   if (window.jQuery) {  
+       // jQuery is loaded  
+       alert("Get to work!");
+   } else {
+    location.reload();
+   }
+}
+
