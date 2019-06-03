4d8f647115265fc2fa5722eff3eb39d3efb30d61
David Welsh <welshdav@msu.edu>
2019-06-03 15:56:43 -0500

    Create delete_top.js

---
+++ delete_top.js
@@ -0,0 +1,13 @@
+        (function() {
+            
+            var bodyEl = $('body'),
+                navToggleBtn = bodyEl.find('.nav-toggle-btn');
+            
+            navToggleBtn.on('click', function(e) {
+                bodyEl.toggleClass('active-nav');
+                e.preventDefault();
+            });
+            
+            
+            
+        })();
